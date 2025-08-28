eb780b5db6e045c71a6a79c6d23d1795db6eced4
Anurag Raj <adityanv8@gmail.com>
2025-08-28 23:30:23 +0530

    Adding Cards

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,15 @@
-export default function RootLayout({children}) {
-  return <div>
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function RootLayout({children}:{children:ReactNode}) {
+  return <div className="root-layout">
+    <nav>
+      <Link href={"/"} className=" flex items-center gap-2">
+      <Image src={'/logo.svg'} alt="logo" width={38} height={32}></Image>
+      <h2 className=" text-primary-100">InterPrep</h2>
+      </Link>
+    </nav>
     {children}
   </div>
 }
